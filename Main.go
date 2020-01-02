ad284d12ab16fa42da272abb4122ad3d65ec1d63
Eoghan Maxwell <eoghan.maxwell@kiwi.com>
2020-01-02 20:18:20 +0000

    Adding hello world endpoint

--- Main.go
+++ Main.go
@@ -2,29 +2,14 @@ package main
 
 import (
 	"fmt"
-	"sync"
+	"io"
+	"net/http"
 )
 
-var wg = sync.WaitGroup{}
-
 func main() {
-	wg.Add(2)
-	ch := make(chan int, 50)
-	go func(ch <-chan int) {
-		for {
-			if i, ok := <-ch; ok {
-				fmt.Println("ok :", i)
-			} else {
-				break
-			}
-		}
-		wg.Done()
-	}(ch)
-	go func(ch chan<- int) {
-		ch <- 42
-		ch <- 27
-		close(ch)
-		wg.Done()
-	}(ch)
-	wg.Wait()
+	http.HandleFunc("/hello", func(w http.ResponseWriter, _ *http.Request) {
+		fmt.Println("Request recived")
+		io.WriteString(w, "Hello World")
+	})
+	http.ListenAndServe(":8080", nil)
 }
