487fb902f35af10a3c7f27588bda7cb34ce512bd
Eoghan Maxwell <eoghan.maxwell@kiwi.com>
2020-01-05 19:13:30 +0000

    WIP for working logging middleware for all requests

--- Main.go
+++ Main.go
@@ -9,19 +9,25 @@ import (
 	"github.com/emaxwell14/go-rest-api/api"
 )
 
-func loggerMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(_ http.ResponseWriter, r *http.Request) {
-		timestamp := time.Now().Format("2006-01-02 15:04:05")
-		fmt.Printf("%s: %s request recieved on endpoint %s\n", timestamp, r.Method, r.URL.Path)
-		next.ServeHTTP(w, r)
-	})
+// Cant get this working
+// func loggerMiddleware(next http.Handler) http.Handler {
+// 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+// 		timestamp := time.Now().Format("2006-01-02 15:04:05")
+// 		fmt.Printf("%s: %s request recieved on endpoint %s\n", timestamp, r.Method, r.URL.Path)
+// 		next.ServeHTTP(w, r)
+// 	})
+// }
+
+func loggerFunc(_ http.ResponseWriter, r *http.Request) {
+	timestamp := time.Now().Format("2006-01-02 15:04:05")
+	fmt.Printf("%s: %s request recieved on endpoint %s\n", timestamp, r.Method, r.URL.Path)
 }
 
 func main() {
 	mux := http.NewServeMux()
-	// api.Tasks(mux)
-	// Only happens on exact path
-	mux.HandleFunc("/", loggerMiddleware(api.Tasks(mux)))
+	api.Tasks(mux)
+
+	mux.HandleFunc("/", loggerFunc) // Only happens on exact path
 	err := http.ListenAndServe(":8080", mux)
 	log.Fatal(err)
 }
