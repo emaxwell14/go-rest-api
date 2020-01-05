29788626dc754f906d12b35d56087c655b2b0ae8
Eoghan Maxwell <eoghan.maxwell@kiwi.com>
2020-01-05 15:56:12 +0000

    Adding middleware for logging

--- Main.go
+++ Main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/emaxwell14/go-rest-api/api"
 )
 
+func loggerMiddleware(_ http.ResponseWriter, r *http.Request) {
+	timestamp := time.Now().Format("2006-01-02 15:04:05")
+	fmt.Printf("%s: %s request recieved on endpoint %s\n", timestamp, r.Method, r.URL.Path)
+}
+
 func main() {
 	mux := http.NewServeMux()
 	api.Tasks(mux)
-
+	// Only happens on exact path
+	mux.HandleFunc("/", loggerMiddleware)
 	err := http.ListenAndServe(":8080", mux)
 	log.Fatal(err)
 }
